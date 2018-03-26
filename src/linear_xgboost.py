fd017e04627ce783bb88dcbd027b50cd87940980
Leo Andeol <leo.andeol@gmail.com>
2018-03-26 17:23:26 +0200

    updated xgboost script by lack of memory

--- src/linear_xgboost.py
+++ src/linear_xgboost.py
@@ -1,8 +1,10 @@
-from sklearn.linear_model import LinearRegression
-from sklearn.model_selection import train_test_split
 import numpy as np
 import pandas as pd
 from xgboost import XGBRegressor
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import PolynomialFeatures
+
+#todo era
 
 print("Loading")
 data_train = pd.read_csv("../data/numerai_training_data.csv")
@@ -12,9 +14,10 @@ X = data_train[features]
 y = data_train.target
 #X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.25)
 X_pred = data_pred[features]
+pipe = make_pipeline(PolynomialFeatures(1,interaction_only=True),XGBRegressor(n_estimators=100, max_depth=4, learning_rate=0.02, subsample=0.9, colsample_bytree=0.85, objective='reg:linear',verbose=5))
 
 print("Training")
-model = XGBRegressor(n_estimators=100, max_depth=4, learning_rate=0.02, subsample=0.9, colsample_bytree=0.85, objective='reg:linear')
+model = pipe
 model.fit(X, y)
 
 print("Predicting...")
