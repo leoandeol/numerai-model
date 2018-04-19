82e6c5725c375d23040b60a76347b1b63cded95d
Léo Andéol <leo.andeol@gmail.com>
2018-04-19 10:15:40 +0200

    linear svm predictions

--- src/linear_svm.py
+++ src/linear_svm.py
@@ -1,16 +1,34 @@
 from sklearn.svm import SVC
-from sklearn.model_selection import train_test_split
 import numpy as np
 import pandas as pd
+from xgboost import XGBRegressor
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import StandardScaler
 
-data = pd.read_csv("../data/numerai_training_data.csv")
+#todo era
+
+print("Loading")
+data_train = pd.read_csv("../data/numerai_training_data.csv")
+data_pred = pd.read_csv("../data/numerai_tournament_data.csv")
 features = ["feature"+str(i+1) for i in range(50)]
-X = data[features]
-y = data.target
-X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.25)
+X = data_train[features]
+y = data_train.target
+#X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.25)
+X_pred = data_pred[features]
+pipe = make_pipeline(StandardScaler(),SVC(kernel='linear'))
 
-model = SVC(verbose=2)
-print("Fitting")
-model.fit(X_train,y_train)
-print("Testing")
-print("Score=",model.score(X_test,y_test))j
+#pipe = make_pipeline(PolynomialFeatures(2),XGBRegressor(n_estimators=100, max_depth=4, learning_rate=0.02, subsample=0.9, colsample_bytree=0.85, objective='reg:linear'))
+
+print("Training")
+model = pipe
+model.fit(X, y)
+
+print("Predicting...")
+# Your trained model is now used to make predictions on the numerai_tournament_data
+results = model.predict(X_pred)
+results_df = pd.DataFrame(data={'probability':results})
+joined = pd.DataFrame(data_pred.id).join(results_df)
+
+print("Writing predictions")
+# Save the predictions out to a CSV file
+joined.to_csv('../predictions/predictions_linear_svm.csv', index=False)
